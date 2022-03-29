d527aac668b83bc594554a95e9597e647855ce1a
cinemafly <102202274+cinemafly@users.noreply.github.com>
2022-03-29 10:15:50 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 basic.forever(function () {
-    led.plotBarGraph(
-    input.lightLevel(),
-    255
-    )
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showNumber(input.lightLevel())
-        basic.pause(100)
+    if (input.lightLevel() < 15) {
+        led.setBrightness(255)
+        basic.showIcon(IconNames.Heart)
+    } else {
+        if (input.lightLevel() < 50) {
+            led.setBrightness(100)
+            basic.showIcon(IconNames.Square)
+        } else {
+            led.setBrightness(20)
+            basic.showIcon(IconNames.SmallHeart)
+        }
     }
 })
